afd62c59b85c62b14231a5f0f7823457cb61cc8d
Leo <yli304@sheffield.ac.uk>
2021-12-07 19:04:56 +0000

    Implement function of selecting photos from Gallery

--- app/src/main/java/uk/ac/shef/oak/com4510/ui/gallery/GalleryFragment.java
+++ app/src/main/java/uk/ac/shef/oak/com4510/ui/gallery/GalleryFragment.java
@@ -1,30 +1,44 @@
 package uk.ac.shef.oak.com4510.ui.gallery;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import uk.ac.shef.oak.com4510.databinding.FragmentNotificationsBinding;
+import uk.ac.shef.oak.com4510.databinding.FragmentGalleryBinding;
+import uk.ac.shef.oak.com4510.R;
 
 public class GalleryFragment extends Fragment {
 
     private GalleryViewModel galleryViewModel;
-    private FragmentNotificationsBinding binding;
+    private FragmentGalleryBinding binding;
+    private ImageView imageView;
+    private Button photoButton;
+    private Button cameraButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         galleryViewModel =
                 new ViewModelProvider(this).get(GalleryViewModel.class);
 
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textNotifications;
@@ -34,6 +48,36 @@ public class GalleryFragment extends Fragment {
                 textView.setText(s);
             }
         });
+        photoButton = root.findViewById(R.id.photo_button);
+        photoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Uri uri;
+                uri = MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL);
+
+
+
+                Intent intent = new Intent(Intent.ACTION_PICK, uri);
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                pickPhotoLauncher.launch(intent);
+            }
+
+            ActivityResultLauncher<Intent> pickPhotoLauncher = registerForActivityResult(
+                    new ActivityResultContracts.StartActivityForResult(),
+                    new ActivityResultCallback<ActivityResult>() {
+
+                        @Override
+                        public void onActivityResult(ActivityResult result) {
+                            if (result.getResultCode() == Activity.RESULT_OK) {
+
+                                Intent data = result.getData();
+
+                                //这里写把图片保存到哪！！！！！！！！
+
+                            }
+                        }});
+        });
+        cameraButton = root.findViewById(R.id.camera_button);
         return root;
     }
 
